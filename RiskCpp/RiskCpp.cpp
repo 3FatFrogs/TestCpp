e0635859aac6cb3ad57ea59fb46dcf54e8746cb5
3FatFrogs <e.stomeo@gmail.com>
2017-02-17 00:40:54 +0000

    Added IntAsSumOfTwoIntegers

--- RiskCpp/RiskCpp.cpp
+++ RiskCpp/RiskCpp.cpp
@@ -1,32 +1,32 @@
 // RiskCpp.cpp : Defines the entry point for the console application.
-//
 
 #include "stdafx.h"
 #include "iostream"
 #include "SimpleLinkedList.h"
 
 using namespace std;
-
-
 //===================================================================================
 class Empty { };
 void f();
-
+bool IntAsSumOfTwoIntegers(int value);
+bool checkPrime(int n);
 //===================================================================================
 int main()
 {
 	struct Node *head = NULL;
 	SimpleLinkedList pippo;
+
 	for (size_t i = 0; i < 10; i++)
 	{
 		pippo.insertHead(&head, i);
 	}
 
-	bool test = pippo.findCircular(head);
-
-	cout << "Test = " << test << endl;
+	pippo.findCircular(head);
 
 	pippo.display(head);
+
+
+	IntAsSumOfTwoIntegers(20);
 	return 0;
 }
 
@@ -44,4 +44,42 @@ void f()
 		cout<< "impossible: report error to compiler supplier";
 }
 
+//===================================================================================
+// Check Whether a Number can be Expressed as a Sum of Two Prime Numbers
+//===================================================================================
+bool IntAsSumOfTwoIntegers(int value)
+{
+	bool flag = false;
 
+	for (int i = 1; i <= value / 2; ++i)
+	{
+		if (checkPrime(i))
+		{
+			if (checkPrime(value - i))
+			{
+				cout << value << " = " << i << " + " << value - i << endl;
+				flag = true;
+			}
+		}
+	}
+
+	return flag;
+}
+
+// Check prime number
+bool checkPrime(int n)
+{
+	int i;
+	bool isPrime = true;
+
+	for (i = 2; i <= n / 2; ++i)
+	{
+		if (n % i == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+
+	return isPrime;
+}
--- RiskCpp/SimpleLinkedList.h
+++ RiskCpp/SimpleLinkedList.h
@@ -5,18 +5,16 @@ struct Node {
 	Node *next;
 };
 
-
 class SimpleLinkedList
 {
-private:
-	Node * head;
-
-public:
-	void display(struct Node *p);
-	int insertHead(struct Node **head, int value);
-	int insertTail(struct Node **head, int value);
-	bool findCircular(Node *head);
-	SimpleLinkedList();
-	~SimpleLinkedList();
+	private:
+		Node * head;
+	public:
+		void display(struct Node *p);
+		int insertHead(struct Node **head, int value);
+		int insertTail(struct Node **head, int value);
+		bool findCircular(Node *head);
+		SimpleLinkedList();
+		~SimpleLinkedList();
 };
 
